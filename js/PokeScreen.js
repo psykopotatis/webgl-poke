c7a087452a978272385f30ef21366394f106e311
iingemar <iingemar@gmail.com>
2016-02-02 15:22:39 +0100

    Add explode on click

--- js/PokeScreen.js
+++ js/PokeScreen.js
@@ -4,6 +4,8 @@ var PokeScreen = Class.extend({
         this.MAX_POKE = 649;
         this.pokeX = -100;
         this.pokeY = 100;
+        this.pokePixels = [];
+        this.exploding = false;
 
         this.stars = starField({
             scene: this.scene,
@@ -12,6 +14,12 @@ var PokeScreen = Class.extend({
         });
 
         this.getRandomPoke();
+
+        var that = this;
+
+        $('#boom a').click(function() {
+            that.createExplosion();
+        })
     },
 
     getRandomPoke: function() {
@@ -44,9 +52,46 @@ var PokeScreen = Class.extend({
         pixel.position.x = position.x;
         pixel.position.y = position.y;
         this.scene.add(pixel);
+        // Save each pixel, so we can explode it later
+        this.pokePixels.push(pixel);
+    },
+
+
+    createExplosion: function() {
+        if (this.exploding) {
+            return;
+        }
+
+        var length = this.pokePixels.length;
+
+        for (var i=0; i<length; i++) {
+            var pixel = this.pokePixels[i];
+            pixel.vx = Math.random() * 0.5 * (Math.random() > 0.5 ? -1 : 1);
+            // pixel.vy = Math.random() * 1.2 * (Math.random() > 0.5 ? -1 : 1);
+            pixel.vz = Math.random() * 3 * (Math.random() > 0.5 ? -1 : 1);
+        }
+
+        this.exploding = true;
     },
 
     update: function() {
         this.stars.update();
+        this.updateExplosion();
+    },
+
+    updateExplosion: function() {
+        if (! this.exploding) {
+            return;
+        }
+
+        var length = this.pokePixels.length;
+
+        for (var i=0; i<length; i++) {
+            var pixel = this.pokePixels[i];
+            pixel.position.x += pixel.vx;
+            // pixel.position.y += pixel.vy;
+            pixel.position.z += pixel.vz;
+            pixel.rotation.x += 0.03;
+        }
     }
 });
