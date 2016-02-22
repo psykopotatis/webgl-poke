80415f718d3c3e5095bd6ed84380c6c664961a68
iingemar <iingemar@gmail.com>
2016-02-22 14:30:41 +0100

    Change db to sqlite

--- project_name/settings.py
+++ project_name/settings.py
@@ -78,18 +78,10 @@ WSGI_APPLICATION = 'project_name.wsgi.application'
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'psy_db',
-        'USER': 'ingemar',
-        'PASSWORD': 'xxx',
-        'HOST': '',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
-# Update database configuration with $DATABASE_URL.
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
